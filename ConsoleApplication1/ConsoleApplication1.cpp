388c88703479354f64da5fc0dec60a3635ea82e3
soydan7419 <128997112+soydan7419@users.noreply.github.com>
2023-05-02 23:17:37 +0300

    Update ConsoleApplication1.cpp

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include <vector>
 #include <string>
 
@@ -45,7 +45,7 @@ public:
         }
     }
     virtual void thirst_control() {
-        if (nutritional_status < 50) {
+        if (there_is_thirst) {
             there_is_thirst = true;
             cout << "thirsty: " << type << endl;
         }
@@ -79,10 +79,10 @@ public:
     void feed() {
         nutritional_status += 10;
     }
-    void health_check() {
+    void health_control() {
         health_situation -= 5;
     }
-    void Show_Information() {
+    void show_information() {
         cout << "type: " << type << endl;
         cout << "age: " << age << endl;
         cout << "Health Situation: " << health_situation << endl;
@@ -91,95 +91,88 @@ public:
         disease_control();
         hunger_control();
         thirst_control();
-        void Show_Information(); {
-            cout << "type: " << type << endl;
-            cout << "age: " << age << endl;
-            cout << "Health Situation: " << health_situation << endl;
-            cout << "Nutritional Status: " << nutritional_status << endl;
-            cout << "milk yield: " << milk_yield << endl;
-            disease_control();
-            hunger_control();
-            thirst_control();
-        }
-    };
+    }
+};
 
-    class Chicken : public Animal {
-    private:
-        int Number_of_Eggs;
-    public:
-        Chicken(int age_) : Animal("Chicken", age_) {
-            Number_of_Eggs = 0;
-        }
-        void food() {
-            nutritional_status += 5;
-        }
-        void health_check() {
-            health_situation -= 2;
-        }
-        void Show_Information() {
-            cout << "type: " << type << endl;
-            cout << "age: " << age << endl;
-            cout << "Health Situation: " << health_situation << endl;
-            cout << "Nutritional Status: " << nutritional_status << endl;
-            cout << "number of eggs: " << Number_of_Eggs << endl;
-            disease_control();
-            hunger_control();
-            thirst_control();
-        }
-        void collect_eggs() {
-            Number_of_Eggs += 1;
-            cout << "eggs were collected." << endl;
-        }
-    };
+class Chicken : public Animal {
+private:
+    int Number_of_Eggs;
+public:
+    Chicken(int age_) : Animal("Chicken", age_) {
+        Number_of_Eggs = 0;
+    }
+    void feed() {
+        nutritional_status += 5;
+    }
+    void health_control() {
+        health_situation -= 2;
+    }
+    void show_information() {
+        cout << "type: " << type << endl;
+        cout << "age: " << age << endl;
+        cout << "Health Situation: " << health_situation << endl;
+        cout << "Nutritional Status: " << nutritional_status << endl;
+        cout << "number of eggs: " <<
+            Number_of_Eggs << endl;
+        disease_control();
+        hunger_control();
+        thirst_control();
+    }
+    void collect_eggs() {
+        Number_of_Eggs += 1;
+        cout << "eggs were collected." << endl;
+    }
+};
 
-    class AgricultureTool {
-    protected:
-        string type;
-        int year;
-    public:
-        AgricultureTool(string type_, int year_) {
-            type = type_;
-            year = year_;
-        }
-        virtual void show_information() = 0;
-    };
+class AgricultureTool {
+protected:
+    string type;
+    int year;
+public:
+    AgricultureTool(string type_, int year_) {
+        type = type_;
+        year = year_;
+    }
+    virtual void show_information() = 0;
+};
 
-    class tractor : public AgricultureTool {
-    private:
-        int fuel_condition;
-    public:
-        tractor(int year_) : AgricultureTool("tractor", year_) {
-            fuel_condition = 100;
-        }
-        void show_information() {
-            cout << "type: " << type << endl;
-            cout << "year: " << year << endl;
-            cout << "fuel_condition: " << fuel_condition << endl;
-        }
-        void refuel(int amount) {
-            fuel_condition += amount;
-            cout << "refueled." << endl;
-        }
-        void plow_soil() {
-            cout << "plow soil." << endl;
-        }
-    };
+class tractor : public AgricultureTool {
+private:
+    int fuel_condition;
+public:
+    tractor(int year_) : AgricultureTool("tractor", year_) {
+        fuel_condition = 100;
+    }
+    void show_information() {
+        cout << "type: " << type << endl;
+        cout << "year: " << year << endl;
+        cout << "fuel_condition: " << fuel_condition << endl;
+    }
+    void refuel(int amount) {
+        fuel_condition += amount;
+        cout << "refueled." << endl;
+    }
+    void plow_soil() {
+        cout << "plow soil." << endl;
+    }
+};
 
-    class Harvester : public AgricultureTool {
-    private:
-        int harvest_status;
-    public:
-        Harvester(int year_) : AgricultureTool("Harvester", year_) {
-            harvest_status = 0;
-        }
-        void show_information() {
-            cout << "type: " << type << endl;
-            cout << "year: " << year << endl;
-            
-        }
-        void refuel(int amount) {
-            cout << "refueled." << endl;
-        }
-        void plow_soil() {
-            cout << "plow soil." << endl;
-        }
+class Harvester : public AgricultureTool {
+private:
+    int harvest_status;
+public:
+    Harvester(int year_) : AgricultureTool("Harvester", year_) {
+        harvest_status = 0;
+    }
+    void show_information() {
+        cout << "type: " << type << endl;
+        cout << "year: " << year << endl;
+
+    }
+    void refuel(int amount) {
+        cout << "refueled." << endl;
+    }
+    void plow_soil() {
+        cout << "plow soil." << endl;
+    }
+};
